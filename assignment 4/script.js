22df1f042f1d0c78d4b854142345bae5b05d2473
vkp99 <vivekprajapatihk108@gmail.com>
2021-06-26 11:23:59 +0530

    update

--- assignment 4/script.js
+++ assignment 4/script.js
@@ -1,14 +1,14 @@
 var names=new Array();
 names[0]="Yaakov";
 names[1]="John";
-names[2]="Jordan";
+names[2]="Jen";
 names[3]="jason";
 names[4]="paul";
-names[5]="vikram";
-names[6]="prashant";
-names[7]="priyanshu";
-names[8]="vivek";
-names[9]="jashmine";
+names[5]="frank";
+names[6]="larry";
+names[7]="paula";
+names[8]="laura";
+names[9]="jim";
 
 
 for (var i = 0; i < names.length; i++) {
